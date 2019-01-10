55fc0d0418fb3b5125c912e6a90182805a2765fc
Periklis <periklas@hotmail.com>
2019-01-10 11:46:01 +0000

    headers | headers update

--- app/services/SessionService.scala
+++ app/services/SessionService.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 HM Revenue & Customs
+ * Copyright 2019 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,13 +17,12 @@
 package services
 
 import config.{ApplicationConfig, RasShortLivedHttpCaching, SessionCacheWiring}
+import models.FileUploadStatus._
 import models._
 import org.joda.time.DateTime
 import play.api.Logger
-import play.api.mvc.Request
-import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.http.cache.client.ShortLivedHttpCaching
-import models.FileUploadStatus._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
